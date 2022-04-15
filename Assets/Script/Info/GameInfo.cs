6c5c7e6e7d9317e4d07af5f835f89b0338632fb8
kkulbeol <chaeyh04@gmail.com>
2022-04-15 23:55:12 +0900

    Firebase

---
+++ Assets/Script/Info/GameInfo.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+/// <summary>
+/// 인게임 플레이어 승리 횟수 정보
+/// </summary>
+[Serializable]
+public struct BestOfCount
+{
+    public int user1;
+    public int user2;
+}
+
+/// <summary>
+/// 매칭 후 게임에 대한 정보를 관리하는 클래스
+/// </summary>
+[Serializable]
+public class GameInfo
+{
+    /// <summary>
+    /// 게임정보를 생성하는 생성자
+    /// </summary>
+    /// <param name="mapType">게임에서 진행되는 맵의 타입</param>
+    /// <param name="user1">게임을 플레이하는 유저1의 정보</param>
+    /// <param name="user2">게임을 플레이하는 유저2의 정보</param>
+    public GameInfo(eMAP_TYPE mapType, UserInfo user1, UserInfo user2)
+    {
+        this.mapType = mapType;
+        this.user1 = user1;
+        this.user2 = user2;
+        id = K.SHA256($"{mapType}{user1.id}{user1.name}{user2.id}{user2.name}{DateTime.Now}");
+    }
+
+    public string id;
+
+    UserInfo user1;
+    UserInfo user2;
+
+    public eMAP_TYPE mapType;
+}
---
+++ Assets/Script/Info/UserInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// 유저의 전적을 관리하는 구조체
+/// </summary>
+[Serializable]
+public struct UserRecordInfo
+{
+    public int win;
+    public int lose;
+}
+
+/// <summary>
+/// 유저의 정보를 관리하는 클래스
+/// </summary>
+[Serializable]
+public class UserInfo
+{
+    public UserInfo(string id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public string id;
+    public string name;
+
+    public UserRecordInfo userRecordInfo;
+
+    public eCHARACTOR_TYPE charactorType;
+}
