681e961cb0099ae51aaf86f0290c2f8333a10630
ysh <pzxcqwsx2@gmail.com>
2022-06-24 11:21:11 +0900

    < update > Title Scene

--- Assets/CYH/Scripts/SignLinker.cs
+++ Assets/CYH/Scripts/SignLinker.cs
@@ -3,30 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using MyPacket;
+using UnityEngine.EventSystems;
 
 public class SignLinker : MonoBehaviour
 {
-    public Window window;
     public InputField inputID;
-    public InputField inputPW;
-    public InputField inputPWAgain;
+    public InputField inputPw;
+    public InputField inputPwAgain;
     public Button btnSign;
+    public Button btnToLogin;
+
+    private void OnEnable()
+    {
+        inputID.Select();
+    }
+
+    private void OnDisable()
+    {
+        inputID.text = string.Empty;
+        inputPw.text = string.Empty;
+        inputPwAgain.text = string.Empty;
+    }
 
     public void Start()
     {
         btnSign.onClick.AddListener(Sign);
 
         inputID.text = "";
-        inputPW.text = "";
-        inputPWAgain.text = "";
+        inputPw.text = "";
+        inputPwAgain.text = "";
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                var prev = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+                if (prev != null) prev.Select();
+            }
+            else
+            {
+                var next = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+                if (next != null) next.Select();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            btnSign.onClick.Invoke();
+        }
     }
 
     public void Sign()
     {
         REQ_Signin req = new REQ_Signin();
         req.id = inputID.text;
-        req.pw = inputPW.text;
-        req.pwAgain = inputPWAgain.text;
+        req.pw = inputPw.text;
+        req.pwAgain = inputPwAgain.text;
 
         K.Send(PacketType.REQ_SIGNIN_PACKET, req);
 
@@ -35,7 +69,7 @@ public class SignLinker : MonoBehaviour
             var res = packet.GetPacket<RES>();
             if (!res.completed) return;
 
-            window.Close();
+            gameObject.SetActive(false);
         };
     }
 }
