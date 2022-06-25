d01da8469a5506c8c918f3697af1c12f61a38701
woalsdl04 <woalsdl0439@gmail.com>
2022-06-26 00:56:05 +0900

    delete | delay

--- Assets/JJM/Script/Caric/State/Attack/Attack.cs
+++ Assets/JJM/Script/Caric/State/Attack/Attack.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MyPacket;
 
 public abstract class Attack : State
 {
--- Assets/JJM/Script/Caric/State/Delay.cs
+++ Assets/JJM/Script/Caric/State/Delay.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MyPacket;
 
 public class Delay : State
 {
@@ -8,8 +9,7 @@ public class Delay : State
     float delayTime = 0;
     public override void Enter()
     {
-        StateInit("", CARIC_STATE.STAND); //애니메이션 안함
-        delayTime = V.worldTime + 0.25f;
+
     }
     public override void Tick()
     {
