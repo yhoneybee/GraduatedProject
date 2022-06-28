54cfbc2164586f6d6a92a676357404bddd3defe6
yhoneybee <chaeyh04@gmail.com>
2022-06-29 01:35:05 +0900

    < update > dll

--- Assets/JJM/Script/Manager/Scene/Ingame.cs
+++ Assets/JJM/Script/Manager/Scene/Ingame.cs
@@ -22,10 +22,10 @@ public class Ingame : SceneBase<Ingame> //인게임 씬
         players[0] = Game.Instance.CreateCaric(players[0], players[0].name, 0, spawnPoints[0].position);
         players[1] = Game.Instance.CreateCaric(players[1], players[1].name, 1, spawnPoints[1].position);
         
-        onStart(this, EventArgs.Empty); //이벤트 호출
     }
     public override void SceneEnter()
     {
+        onStart(this, EventArgs.Empty); //이벤트 호출
     }
  
     public override void ScenePlaying()
