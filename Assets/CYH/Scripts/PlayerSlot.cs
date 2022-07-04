8c89a20bbc8e945abee00c337dfd719b35fcad80
yhoneybee <chaeyh04@gmail.com>
2022-07-05 00:53:08 +0900

    < update > hanguel

--- Assets/CYH/Scripts/PlayerSlot.cs
+++ Assets/CYH/Scripts/PlayerSlot.cs
@@ -9,7 +9,7 @@ public class PlayerSlot : MonoBehaviour
     public void Set(UserInfo userInfo)
     {
         txtPlayer.text = userInfo.id;
-        txtInfo.text = txtInfo.text = $"Win : {userInfo.win:D5}\nLose : {userInfo.lose:D5}";
+        txtInfo.text = txtInfo.text = $"½Â¸® : {userInfo.win:D5}\nÆÐ¹è : {userInfo.lose:D5}";
         txtReady.text = string.Empty;
     }
 
