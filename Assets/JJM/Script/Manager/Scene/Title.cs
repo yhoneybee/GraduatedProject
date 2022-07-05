c9c2d2607252bda7b2f062245a1679130f362fe0
woalsdl04 <woalsdl0439@gmail.com>
2022-07-05 13:18:17 +0900

    update | delete dotween

--- Assets/JJM/Script/Manager/Scene/Title.cs
+++ Assets/JJM/Script/Manager/Scene/Title.cs
@@ -5,12 +5,8 @@ using DG.Tweening;
 
 public class Title : SceneBase<Title> //≈∏¿Ã∆≤ æ¿ ≈¨∑°Ω∫
 {
-    public GameObject loginObj;
-
-    public int Step; // æ¿ Ω∫≈‹
     public override void SceneAwake()
     {
-        if(loginObj == null) loginObj = GameObject.Find("Login");
     }
 
     public override void SceneStart()
@@ -24,26 +20,7 @@ public class Title : SceneBase<Title> //≈∏¿Ã∆≤ æ¿ ≈¨∑°Ω∫
 
     public override void ScenePlaying()
     {
-        switch (Step) 
-        {
-            case 0:
 
-                if (Input.anyKeyDown)
-                {
-                    Step = 1;
-                    loginObj.GetComponent<RectTransform>().DOAnchorPos(new Vector3(0,0,0), 1, false);
-                    break;
-                }
-
-                break;
-            case 1:
-
-
-
-                break;
-            case 2:
-                break;
-        }
     }
 
     public override void SceneEnd()
